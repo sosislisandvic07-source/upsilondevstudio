df238b030d6e5450eee80e81319216a62c38083c
UpsilonDevStudio <sosislisandvic07@gmail.com>
2025-11-22 00:25:25 +0300

    Update lightbox.js

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,24 +1,19 @@
-/* ============================================
-   LIGHTBOX (Görsel Büyütme Sistemi)
-============================================ */
-
 function initLightbox() {
-
-    // Lightbox HTML ekleme
     const lightbox = document.createElement("div");
     lightbox.id = "lightbox";
+
     lightbox.innerHTML = `
         <div id="lightbox-bg"></div>
         <img id="lightbox-img" src="">
         <span id="lightbox-close">❌</span>
     `;
+
     document.body.appendChild(lightbox);
 
     const bg = document.getElementById("lightbox-bg");
     const img = document.getElementById("lightbox-img");
-    const closeBtn = document.getElementById("lightbox-close");
+    const close = document.getElementById("lightbox-close");
 
-    // Tüm portföy görselleri
     document.querySelectorAll(".slide-img").forEach((image) => {
         image.addEventListener("click", () => {
             img.src = image.src;
@@ -26,8 +21,6 @@ function initLightbox() {
         });
     });
 
-    // Kapatma
     bg.addEventListener("click", () => lightbox.classList.remove("active"));
-    closeBtn.addEventListener("click", () => lightbox.classList.remove("active"));
+    close.addEventListener("click", () => lightbox.classList.remove("active"));
 }
-
