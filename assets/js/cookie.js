f421142ae4bacba56def1e68f08a850fe18d0ace
UpsilonDevStudio <sosislisandvic07@gmail.com>
2025-11-22 00:25:56 +0300

    Update cookie.js

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,22 +1,13 @@
-/* ============================================
-   COOKIE BANNER (Çerez Bildirimi Sistemi)
-============================================ */
-
 document.addEventListener("DOMContentLoaded", () => {
-
     const banner = document.getElementById("cookie-banner");
-    const acceptBtn = document.getElementById("acceptCookies");
+    const accept = document.getElementById("acceptCookies");
 
-    // Eğer kullanıcı daha önce kabul ettiyse banner'ı gizle
     if (localStorage.getItem("cookiesAccepted") === "yes") {
         banner.style.display = "none";
     }
 
-    // Kabul edilirse kayıt et + gizle
-    acceptBtn.addEventListener("click", () => {
+    accept.addEventListener("click", () => {
         localStorage.setItem("cookiesAccepted", "yes");
         banner.style.display = "none";
     });
-
 });
-
