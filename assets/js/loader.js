2b0d7baf2426264450dc177487030053e6dc58d8
UpsilonDevStudio <sosislisandvic07@gmail.com>
2025-11-22 00:04:58 +0300

    Update loader.js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,1 +1,18 @@
+/* ============================================
+   LOADER (Yüklenme Ekranı)
+============================================ */
+
+window.addEventListener("load", () => {
+    const loader = document.getElementById("loader");
+
+    setTimeout(() => {
+        loader.style.opacity = "0";
+        loader.style.pointerEvents = "none";
+
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 500);
+
+    }, 500); // 0.5 saniye bekleme süresi (isteğe bağlı)
+});
 
